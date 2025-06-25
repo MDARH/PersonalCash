b2a07f2270d3491a00367e4a018c7f265b59c7ea
Hassan Laptop-7400 <mdarh411@gmail.com>
2025-06-25 19:37:54 +0600

    Fix TotalLoanGivenWidget text, UI and icon

---
+++ app/Filament/Widgets/TotalLoanGivenWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Transaction;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+/**
+ * Widget to display the total amount of loans given
+ * Shows the sum of all transactions with type 'loan_given'
+ */
+class TotalLoanGivenWidget extends BaseWidget
+{
+    protected static ?string $pollingInterval = '10s';
+
+    protected function getStats(): array
+    {
+        // Calculate total loan given amount
+        $totalLoanGiven = Transaction::query()
+            ->where('type', 'loan_given')
+            ->sum('amount');
+
+        // Format the amount with Bengali currency symbol
+        $formattedAmount = '৳ ' . number_format($totalLoanGiven, 2);
+
+        return [
+            Stat::make('মোট ঋণ প্রদান', $formattedAmount)
+                ->description('সর্বমোট ঋণ প্রদানের পরিমাণ')
+                ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->color('warning'), // Using the same color as defined in TransactionType enum
+        ];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 \App\Filament\Widgets\TotalLoanTakenWidget::class,
+                \App\Filament\Widgets\TotalLoanGivenWidget::class,
                 \App\Filament\Widgets\DueListOverview::class,
             ])
             ->middleware([
